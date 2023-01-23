6eb190a385307f29208efd213a176fec17ba2f90
Gustavo Ferreira <gfmb94@gmail.com>
2023-01-23 18:25:41 -0300

    First component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Icon from './components/NotificationButton/index'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Icon />
+    <Icon />
   </React.StrictMode>,
 )
